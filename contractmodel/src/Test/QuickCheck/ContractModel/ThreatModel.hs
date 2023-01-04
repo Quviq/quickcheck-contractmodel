43e54c7b92002770df9ca1f66f31651d7fa585dd
Maximilian Algehed <m.algehed@gmail.com>
2023-01-04 15:23:36 +0100

    wip

--- contractmodel/src/Test/QuickCheck/ContractModel/ThreatModel.hs
+++ contractmodel/src/Test/QuickCheck/ContractModel/ThreatModel.hs
@@ -545,8 +545,8 @@ doubleSatisfaction = do
 
   -- add safe script input with protected output, redirect original output to signer
   let safeScript  = alwaysTrueValidator
-      unitDatum   = inlineDatum $ toScriptData ()
-      uniqueDatum = inlineDatum $ toScriptData ("SuchSecure" :: BuiltinByteString)
+      unitDatum   = TxOutDatumHash ScriptDataInBabbageEra (hashScriptData  $ toScriptData ())
+      uniqueDatum = TxOutDatumHash ScriptDataInBabbageEra (hashScriptData $ toScriptData ("SuchSecure" :: BuiltinByteString))
 
       victimTarget = targetOf output
 
